4e96b999e6f42ed9cabe5b58135d1b00ea2f917f
Unknown <robert.a.nickel@gmail.com>
2018-10-22 20:57:59 +1300

    22-10 AnimBuild
    Compiled animations into one scene and made controls for each with final build

---
+++ Unity/Robert Nickel Animation Assignment/Assets/2DStuff/AnimControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour {
+
+    public Animator charCtrl;
+    
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+        {
+            charCtrl.SetFloat("Blend", 1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            charCtrl.SetFloat("Blend", -1);
+        }
+        else
+        {
+            charCtrl.SetFloat("Blend", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            charCtrl.Play("Action");
+        }
+	}
+}
---
+++ Unity/Robert Nickel Animation Assignment/Assets/3DStuff/otherControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class otherControl : MonoBehaviour
+{
+
+    public Animator otherCtrl;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            otherCtrl.SetFloat("Blend", 1);
+        }
+        else
+        {
+            otherCtrl.SetFloat("Blend", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            otherCtrl.Play("Action");
+        }
+    }
+}
